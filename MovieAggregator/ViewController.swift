c524695bc932d4cbdf7a44abd40709582f323dc2
quavodanceq <kuat.oralbekov1011@mail.ru>
2024-07-10 17:23:19 +0500

    Initial Commit

---
+++ MovieAggregator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAggregator
+//
+//  Created by Куат Оралбеков on 10.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
