db6b99f0598835d6d1f7e527d13aaac9b713153c
quavodanceq <kuat.oralbekov1011@mail.ru>
2024-07-13 17:35:10 +0500

    custom movies feed view

---
+++ MovieAggregator/Views/CustomCell.swift
@@ -0,0 +1,8 @@
+//
+//  CustomCell.swift
+//  MovieAggregator
+//
+//  Created by Куат Оралбеков on 13.07.2024.
+//
+
+import Foundation
---
+++ MovieAggregator/Views/CustomTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTableViewCell.swift
+//  MovieAggregator
+//
+//  Created by Куат Оралбеков on 13.07.2024.
+//
+
+import Foundation
