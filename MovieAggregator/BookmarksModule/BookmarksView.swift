c0630e3bf9eeecf3ac1f54312c0e5014b112612a
Kuat <80914126+quavodanceq@users.noreply.github.com>
2024-09-14 11:10:00 +0500

    upd: Bookmarks View

--- MovieAggregator/BookmarksModule/BookmarksView.swift
+++ MovieAggregator/BookmarksModule/BookmarksView.swift
@@ -22,6 +22,10 @@ class BookmarksView: UIView, BookmarksViewInput {
     
     weak var output: BookmarksViewOutput?
     
+    private var collectionView: UICollectionView!
+    
+    private var movies: [Movie] = []
+    
     init() {
         super.init(frame: .zero)
     }
@@ -30,6 +34,51 @@ class BookmarksView: UIView, BookmarksViewInput {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setupCollectionView() {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+        addSubview(collectionView)
+    }
+    
+}
+
+extension BookmarksView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        movies.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+        cell.setup(movies[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (collectionView.frame.width / 2)
+        return CGSize(width: size, height: size * 1.2 )
+    }
+    
+   
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//            let product = products[indexPath.row]
+//            presenter?.showProductPage(product: product)
+//    }
     
     
 }
